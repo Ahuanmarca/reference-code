711d16ce7ee677a557dbc9eca54df10f3fd7e78b
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-02-26 17:48:52 +0100

    Add example data-sets to practice using databases

---
+++ EXAMPLES/Node/colors-test/colors.js
@@ -0,0 +1,10 @@
+import colors from 'colors';
+console.log('hello, world'.pink);
+console.log('hello, world'.magenta);
+
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+
