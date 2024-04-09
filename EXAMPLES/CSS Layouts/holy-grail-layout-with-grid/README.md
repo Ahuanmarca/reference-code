81299ee65b0910e0695d0fca06d9eb1df7f717a8
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-09 15:47:41 +0200

    Add CSS Layouts examples

---
+++ EXAMPLES/CSS Layouts/holy-grail-layout-with-grid/README.md
@@ -0,0 +1,4 @@
+# Holy Grail Layout with Grid
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chriscoyier/pen/ZEpPWKO](https://codepen.io/chriscoyier/pen/ZEpPWKO).
+
--- EXAMPLES/npm packages/date-fns.js
+++ EXAMPLES/npm packages/date-fns.js
@@ -1,39 +1,20 @@
 // date-fns examples
-import { format, isValid, isBefore, isSameMonth } from "date-fns";
+import { format, isBefore, isSameMonth } from "date-fns";
 
 // Format current date in yyyy-mm-dd format
 format(new Date(), "yyyy-MM-dd");
 
-// Check if a string is a valid date
-// Will consider any day up to 31 as a valid date, for any month
-isValid(new Date('foo'));
-isValid(new Date('2024-02-29'));
-
-function isValidDate(dateString: any) {
-    // Regular expression to match 'yyyy-mm-dd' format
-    var regex = /^\d{4}-\d{2}-\d{2}$/;
-
-    // Check if the string matches the regex
-    if (!regex.test(dateString)) {
-        return false;
-    }
-
-    // Parse the string into a Date object
-    var dateObject = new Date(dateString);
-
-    // Check if the dateObject is a valid date
-    // and if the parsed year, month, and day match the input string
-    return !isNaN(dateObject.getTime()) &&
-        dateObject.toISOString().slice(0, 10) === dateString;
-}
-
-isValidDate('2020-02-29'); // true
-isValidDate('2021-02-29'); // false (2021 is not a leap year)
-isValidDate('hello');
-isValidDate('17-12-1981');
-isValidDate(undefined);
-
+// Chech if some date is before / after another
 isBefore(new Date('2023-02-01'), new Date('2023-03-01'));
 isBefore(new Date('2023-02-01'), new Date('2023-01-01'));
 isSameMonth(new Date('2023-02-01'), new Date('2023-02-15'));
 isSameMonth(new Date('2023-02-01'), new Date('2023-03-15'));
+
+// 1) Get current date
+const currentDate = new Date();
+const end = format(currentDate, "yyyy-MM-dd"); `end: ${end}`;
+currentDate.setDate(1);
+currentDate.setMonth(currentDate.getMonth() - 11);
+const start = format(currentDate, "yyyy-MM-dd"); `start: ${start}`;
+
+
